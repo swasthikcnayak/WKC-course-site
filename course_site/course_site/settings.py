74d6c460a026731c7483f0f7d5322ed4f8d8a609
swasthik <swasthik.nayak@hevodata.com>
2022-05-28 14:42:54 +0530

    requirements.txt setup

--- course_site/course_site/settings.py
+++ course_site/course_site/settings.py
@@ -167,6 +167,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_dir')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
